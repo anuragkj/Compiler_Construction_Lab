ed35e783c4e30f299f4f05b4797ac06dfba49f37
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2023-03-10 10:16:39 +0400

    Add files via upload

---
+++ Lab_5/input.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int x,y,z;//these are the variables
+	
+	/*In the following section we read 3 variables*/
+	
+	scanf("%d",&x);
+	
+	scanf("%d",&y);
+	
+	scanf("%d",&z);
+	
+	printf("sum=%d",x+y+z);//Output the sum 
+
+}
